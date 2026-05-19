8d2d98a0f104e8af904d89cf3c7f47491a5bdb61
parhelia512 <0011d3@gmail.com>
2026-05-19 20:11:27 +0800

    Update parentcheck_windows.go

--- core/server/parentcheck/parentcheck_windows.go
+++ core/server/parentcheck/parentcheck_windows.go
@@ -8,6 +8,53 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+func getDrivePath(devicePath string) string {
+	size, err := windows.GetLogicalDriveStrings(0, nil)
+	if err != nil || size == 0 {
+		return devicePath
+	}
+
+	buf := make([]uint16, size)
+	_, err = windows.GetLogicalDriveStrings(size, &buf[0])
+	if err != nil {
+		return devicePath
+	}
+
+	var drives []string
+	start := 0
+	for i := 0; i < len(buf); i++ {
+		if buf[i] == 0 {
+			if i == start {
+				break
+			}
+			drive := windows.UTF16ToString(buf[start:i])
+			drive = strings.TrimSuffix(drive, `\`)
+			if drive != "" {
+				drives = append(drives, drive)
+			}
+			start = i + 1
+		}
+	}
+
+	for _, drive := range drives {
+		targetBuf := make([]uint16, windows.MAX_PATH)
+		drivePtr, _ := windows.UTF16PtrFromString(drive)
+
+		targetSize, err := windows.QueryDosDevice(drivePtr, &targetBuf[0], uint32(len(targetBuf)))
+		if err != nil || targetSize == 0 {
+			continue
+		}
+
+		targetDevice := windows.UTF16ToString(targetBuf)
+
+		if strings.HasPrefix(devicePath, targetDevice) {
+			return drive + strings.TrimPrefix(devicePath, targetDevice)
+		}
+	}
+
+	return devicePath
+}
+
 func getParentExePath(pid int) (string, error) {
 	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
 	if err != nil {
@@ -20,5 +67,6 @@ func getParentExePath(pid int) (string, error) {
 	if err := windows.QueryFullProcessImageName(h, 0, &buf[0], &size); err != nil {
 		return "", fmt.Errorf("QueryFullProcessImageName: %w", err)
 	}
-	return windows.UTF16ToString(buf[:size]), nil
+	
+	return getDrivePath(windows.UTF16ToString(buf[:size])), nil
 }
